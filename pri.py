c6dd02426aa4e80cd3831f9368e0ab6b155c8f53
Ahmed Saeed <151210138+ahmed0ss@users.noreply.github.com>
2025-08-11 13:24:35 +0100

    Update pri.py

--- pri.py
+++ pri.py
@@ -1,10 +1,1 @@
-import bcrypt
 
-passwords = {
-    "admin1": "adminpassword",
-    "staff1": "staffpassword"
-}
-
-for username, plain_password in passwords.items():
-    hashed = bcrypt.hashpw(plain_password.encode(), bcrypt.gensalt())
-    print(f'"username": "{username}", "password": "{hashed.decode()}"')
