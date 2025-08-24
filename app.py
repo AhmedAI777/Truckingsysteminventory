d268f586db89ba9b85ad3cdc9bfc3924febc3499
Ahmed Saeed <151210138+AhmedAI777@users.noreply.github.com>
2025-08-24 08:40:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -318,7 +318,61 @@ def _load_sa_info() -> dict:
       - env GOOGLE_SERVICE_ACCOUNT_JSON (JSON string)
     """
     sa = st.secrets["gcp_service_account"]
-    sa["private_key"] = sa["private_key"].replace("\n", "\n")
+    sa[private_key = """-----BEGIN PRIVATE KEY-----
+\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDbUw5JakRNW47X
+\njAKWa8xkBHl5+Z8I4q5z6vK2UhAjOcJWKBHNB+XvWDUowr0d2FYmrvSfbIVK3K7w
+\n1xncl0t5OwDx1quJKcoZnUoirf5mPkbiPw6FNAT3Gzdyyts7ChShPNHbKWOvzrI2
+\nwQzaeA+M+sRQ18lEShtzSqCtDuRgwL6YhRzj9WeFlbZXnR+jLA1E2KMfJT2TTmC5
+\nTN+BWBzXRsXuYCl+B1s+zuIPPDXyv0p1z34pNtJb4fmzoyToSSU4FYbfw3YXu2+C
+\new//YLGpKBG/X8PudSWg4c9Lsamczv+I30m8tRNEIHmTJQVpPS/N3ZJQ9Ey+rSpz
+\n0Y/F058zAgMBAAECggEAXI+sX2J4SfeCjMhbjTUYcPuMcuRc8GiOfIBjB3wRsHQf
+\nZrIJdTDIox7kbHvnSXG0RiYfOisYA/Sn5h+5m+XEJfk3WFkjUsNutimyEHnC/E57
+\nJt+61o+SKuFzIMCpDj0eYL/kxywsFJXUk5QcwxTZZ0Or13yCRg5KkHkl33OCAax+
+\njoGUtW7O254l2Ued+V5Gpfv7LKOlANp/a68wjoW5cn4aGQcyNQxL2nelXSvSjir1
+\n7YVpx5thVuSpRzjm6wznSwY2caf5Kn6Gn0Kc52U8/6r6olWhf0WLEIbbmqOtwnE0
+\nM8SAqOVBBX5bpDG18y3EERS8FVAxDiXdNWu3j6IH7QKBgQDzn3AMCHKG7c2WoXal
+\nBs/oI2vtLLYUvE/uULxpLlMkxPBqBCEZs/MToWblQsonzzvEHqiyikv9wBFL+Fy5
+\nwrU9BSlGxKHOoP2aJUdnoXLaDmZUf3ojkbMnT3nx47/OF21j1lHo1x5EJ79MlBu6
+\ncjRRZvaKqUwBHRD8Gfs8L0695wKBgQDmd5fY925Yax9qRGgjCHb30RCpL9V4oQTg
+\ninZi9GZkAWdL1PuxCmp1/QNvRU4oy5ygHiuG5F3GAVPYRtJ/kL4PI3TaAv90sRaF
+\nWtIlmzb0TYW+z5g2gooTjM4gMN2nNNjukayItOxWZlfmd4wmMpMpQ4rwOAtiTzFs
+\nCH+YgfOy1QKBgF+lnxX6UwyKXIbhCXWtAP9AuOS7AxmM/UyxQeeBmn77GvBkgqJW
+\ntf5lBcLIwBl1ER/kcZL3HPKY77GF5tG/kexNFHGGTYiUSDy2mhwjlLXrpV1TVx6T
+\n22R5nYTMR8egBwCFak8h9e4INODZ3TEMGWJELFMwOHjPcpWnla2BXUbNAoGBALQK
+\nBzCykpw2CxOcHvIHQdD0nJxextf2ifXTlQpWvMoxIn3mAz1Z0rMblZxOOvG5pkCb
+\ncQtuySbOkK5rHTQUYbU30KgjIWcKlHpW6cYBDBwrl2jpiZJDxhPhsoEJS468xR8R
+\n5APjuqEAUHi1OWH5rmbU4ewpDBOfpA8uUGdWVYeFAoGAVSobeFTS3zImrkQzqkhK
+\neIlPOcJAzpv4bK/K0A/kgbRQvAJ/W/ybDfbnnPk9tibDfBz3Kuh4NTPxE9PUTSvc
+\n5tc+7tik8j8XN9t1zNCZtfU4C/5efuD0g96x0zsBPwEsRjNTQLVQJtyhItssv+tI
+\n92jEgIErZzvy2Ny/BUYx2eM=\n
+-----END PRIVATE KEY-----\n"""] = sa[private_key = """-----BEGIN PRIVATE KEY-----
+\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDbUw5JakRNW47X
+\njAKWa8xkBHl5+Z8I4q5z6vK2UhAjOcJWKBHNB+XvWDUowr0d2FYmrvSfbIVK3K7w
+\n1xncl0t5OwDx1quJKcoZnUoirf5mPkbiPw6FNAT3Gzdyyts7ChShPNHbKWOvzrI2
+\nwQzaeA+M+sRQ18lEShtzSqCtDuRgwL6YhRzj9WeFlbZXnR+jLA1E2KMfJT2TTmC5
+\nTN+BWBzXRsXuYCl+B1s+zuIPPDXyv0p1z34pNtJb4fmzoyToSSU4FYbfw3YXu2+C
+\new//YLGpKBG/X8PudSWg4c9Lsamczv+I30m8tRNEIHmTJQVpPS/N3ZJQ9Ey+rSpz
+\n0Y/F058zAgMBAAECggEAXI+sX2J4SfeCjMhbjTUYcPuMcuRc8GiOfIBjB3wRsHQf
+\nZrIJdTDIox7kbHvnSXG0RiYfOisYA/Sn5h+5m+XEJfk3WFkjUsNutimyEHnC/E57
+\nJt+61o+SKuFzIMCpDj0eYL/kxywsFJXUk5QcwxTZZ0Or13yCRg5KkHkl33OCAax+
+\njoGUtW7O254l2Ued+V5Gpfv7LKOlANp/a68wjoW5cn4aGQcyNQxL2nelXSvSjir1
+\n7YVpx5thVuSpRzjm6wznSwY2caf5Kn6Gn0Kc52U8/6r6olWhf0WLEIbbmqOtwnE0
+\nM8SAqOVBBX5bpDG18y3EERS8FVAxDiXdNWu3j6IH7QKBgQDzn3AMCHKG7c2WoXal
+\nBs/oI2vtLLYUvE/uULxpLlMkxPBqBCEZs/MToWblQsonzzvEHqiyikv9wBFL+Fy5
+\nwrU9BSlGxKHOoP2aJUdnoXLaDmZUf3ojkbMnT3nx47/OF21j1lHo1x5EJ79MlBu6
+\ncjRRZvaKqUwBHRD8Gfs8L0695wKBgQDmd5fY925Yax9qRGgjCHb30RCpL9V4oQTg
+\ninZi9GZkAWdL1PuxCmp1/QNvRU4oy5ygHiuG5F3GAVPYRtJ/kL4PI3TaAv90sRaF
+\nWtIlmzb0TYW+z5g2gooTjM4gMN2nNNjukayItOxWZlfmd4wmMpMpQ4rwOAtiTzFs
+\nCH+YgfOy1QKBgF+lnxX6UwyKXIbhCXWtAP9AuOS7AxmM/UyxQeeBmn77GvBkgqJW
+\ntf5lBcLIwBl1ER/kcZL3HPKY77GF5tG/kexNFHGGTYiUSDy2mhwjlLXrpV1TVx6T
+\n22R5nYTMR8egBwCFak8h9e4INODZ3TEMGWJELFMwOHjPcpWnla2BXUbNAoGBALQK
+\nBzCykpw2CxOcHvIHQdD0nJxextf2ifXTlQpWvMoxIn3mAz1Z0rMblZxOOvG5pkCb
+\ncQtuySbOkK5rHTQUYbU30KgjIWcKlHpW6cYBDBwrl2jpiZJDxhPhsoEJS468xR8R
+\n5APjuqEAUHi1OWH5rmbU4ewpDBOfpA8uUGdWVYeFAoGAVSobeFTS3zImrkQzqkhK
+\neIlPOcJAzpv4bK/K0A/kgbRQvAJ/W/ybDfbnnPk9tibDfBz3Kuh4NTPxE9PUTSvc
+\n5tc+7tik8j8XN9t1zNCZtfU4C/5efuD0g96x0zsBPwEsRjNTQLVQJtyhItssv+tI
+\n92jEgIErZzvy2Ny/BUYx2eM=\n
+-----END PRIVATE KEY-----\n"""].replace("\n", "\n")
 
     raw = st.secrets.get("gcp_service_account", {})
     if isinstance(raw, dict):
